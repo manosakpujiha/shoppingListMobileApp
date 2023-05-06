404853cd1495dc769588025750061f5b115769c3
manosakpujiha <manos.akpujiha@gmail.com>
2023-05-06 03:29:01 -0400

    fix

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 import {initializeApp} from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js'
 import {getDatabase, ref, push, onValue, remove} from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
-
 const appSettings = {
     databaseURL : 'https://playground-f1eed-default-rtdb.firebaseio.com/'
 }
 
-let todos = document.querySelector('#list');
-let addBtn = document.querySelector('#add-button');
-let input = document.querySelector('#input-field');
-addBtn.addEventListener('click' , submitToDb);
-const notification = document.querySelector('#notification')
+const outline = document.querySelector('.outline');
+outline.innerHTML = `
+        <div id="toast">
+            <div id="img"><img alt="toast image" src='./jiggly.png' width='50px' height='50px' /></div>
+            <div id="desc">An new item was added by ...</div>
+        </div>
+        <div class="container">
+            <img src="./pokemon-png-image-famous-anime-character-png-only-6.png" alt="pokemon">
+        </div>
+        <input type="text" id="input-field" placeholder="Bread"/>
+        <button id="add-button">Add to cart</button>
+        
+        <ul id="list"></ul>
+    `
+
+const todos = document.querySelector('#list');
+const addBtn = document.querySelector('#add-button');
+const input = document.querySelector('#input-field');
+
 input.addEventListener('keydown', handleSubmit);
-notification.addEventListener('click' , launch_toast);
+addBtn.addEventListener('click' , submitToDb);
+
 
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
@@ -19,16 +33,15 @@ const shoppingListInDB = ref(database, 'shoppingList');
 
 onValue(shoppingListInDB, function(snapshot){
     todos.innerHTML = '';
-
     if (snapshot.exists()) {
         let items = Object.entries(snapshot.val())
         items.map((item) => { 
-            let button = document.createElement('li')
+            const button = document.createElement('li')
             button.innerText=item[1];
             let deleteItem = del.bind(item);
             button.addEventListener('click', deleteItem);
             todos.append(button);
-            launch_toast()
+            launch_toast('Welcome!')
         })
     } else {
         todos.innerHTML= 'Your shopping list is empty!';
@@ -37,12 +50,15 @@ onValue(shoppingListInDB, function(snapshot){
 
 function del() {
     remove(ref(database, `shoppingList/${this[0]}`));
+    launch_toast(`${this[1]} was removed by ...`, 'red')
+    
 }
 
 function submitToDb() {
     if (input.value) {
         push(shoppingListInDB, input.value);
-        runToast();
+        launch_toast(`${input.value} was added by ...`, 'green')
+        
     }
     input.value = '';
 }
@@ -50,6 +66,7 @@ function submitToDb() {
 function handleSubmit(e) {
     if (e.key === 'Enter') {
       submitToDb();
+
     }
   }
 
@@ -66,13 +83,10 @@ function handleSubmit(e) {
         }).showToast();
   }
 
-  function launch_toast() {
+  function launch_toast(text, color='green') {
     let x = document.getElementById("toast");
-
-    console.log(x.className )
-
-    x.className = "show";
-    console.log(x.className )
-
-    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 5000);
+    x.innerText = text;
+    // x.background = color;
+    x.className = `show ${color}`;
+    setTimeout(function(){ x.className = x.className.replace(`show ${color}`, ""); }, 5000);
 }
